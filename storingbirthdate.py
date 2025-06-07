462021dc816e427d4ae8106f7f783c1ea356b72d
Parivartan0koju <parivartankoju2@gmail.com>
2025-06-07 18:50:00 +0545

    Storing birthday

---
+++ storingbirthdate.py
@@ -0,0 +1,10 @@
+A="Birthday of Myself 12/17"
+B="Brithday of Sister1 02/09"
+C="Birthday of Sister2 09/12"
+D="Birthday of Friend 10/11"
+E="Birthday of Brother 06/20"
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
